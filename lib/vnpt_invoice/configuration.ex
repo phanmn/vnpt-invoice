988a81b5d74bbe2084d7a3daa15a4e68d1446f6d
Nhut Phan <thanquan@gmail.com>
2022-08-09 12:56:44 +0700

    feat: Add get(:username) get(:password) to VnptInvoice.Configuration

--- lib/vnpt_invoice/configuration.ex
+++ lib/vnpt_invoice/configuration.ex
@@ -2,4 +2,12 @@ defmodule VnptInvoice.Configuration do
   def get(:origin) do
     Application.get_env(:vnpt_invoice, :origin)
   end
+
+  def get(:username) do
+    Application.get_env(:vnpt_invoice, :username)
+  end
+
+  def get(:password) do
+    Application.get_env(:vnpt_invoice, :password)
+  end
 end
